5fa8f02e16b7d2868b0223267282eda28d2cde14
zbr12313 <69333475+zbr12313@users.noreply.github.com>
2020-08-22 19:25:07 +0800

    update

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+<a class="navbar-brand" href=""><img src="" class="img-fluid rounded logo-img mr-2" alt="LOGO" />欢迎来到小助手娱乐网站！</a>
+
+?>
+
+
+
+
+<center><font size="7">欢迎来到小助手娱乐网站！</font></center>
+<center><fonr size="3">本站只供娱乐，如有版权问题联系QQ：248757761解决！</font></center>
+<center><font size="4"></font></center>
+<br>
+<br>
+<center><font size="5"><a href="./about-ours/index.html"target="_blank">关于我们</a></font></center>
+<br>
+<br>
+<center><font size="5" color="#FF1493"><a href="./B站镇站之宝/index.html">B站镇站之宝系列</a></font></center>
